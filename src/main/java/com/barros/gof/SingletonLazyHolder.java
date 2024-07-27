1ad68454490af95e391f24abedbf1c9abe96edaa
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:06:45 -0300

    Added Singleton Lazy Holder class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/gof/SingletonLazyHolder.java
+++ src/main/java/com/barros/gof/SingletonLazyHolder.java
@@ -1,2 +1,22 @@
-package com.barros.gof;public class SingletonLazyHolder {
+package com.barros.gof;
+
+/**
+ * Singleton Lazy Holder
+ *
+ * @see <a href="https://stackoverflow.com/a/24018148"> Stackoverflow - Lazy Holder </a>
+ *
+ * @author Barros313
+ */
+public class SingletonLazyHolder {
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instance = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstance() {
+        return InstanceHolder.instance;
+    }
 }
