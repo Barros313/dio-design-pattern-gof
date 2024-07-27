1933bacf0de166960173f14ca0ee96e1aa42b48b
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:11:46 -0300

    Added Test class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ src/main/java/com/barros/gof/Test.java
@@ -0,0 +1,27 @@
+package com.barros.gof;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Testing Singleton Lazy");
+
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+
+        System.out.println("Testing Singleton Eager");
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        System.out.println("Testing Singleton Lazy Holder");
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+
+    }
+}
