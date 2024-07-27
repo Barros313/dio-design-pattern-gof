990391ddcbdabbc920f3ac7c1fa82f51895906a1
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:52:13 -0300

    Added CepApi example
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/subsistema2/cep/CepApi.java
+++ src/main/java/subsistema2/cep/CepApi.java
@@ -1,2 +1,21 @@
-package subsistema2.cep;public class CepApi {
+package subsistema2.cep;
+
+public class CepApi {
+    private static CepApi instance = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstance() {
+        return instance;
+    }
+
+    public String getCityThroughPostalCode(String postalCode) {
+        return "Recife";
+    }
+
+    public String getStateThroughPostalCode(String postalCode) {
+        return "PE";
+    }
 }
