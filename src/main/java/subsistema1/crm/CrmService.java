3d29c9e0dcf1543eb9764c1c26cc9e054abd1b4d
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:52:01 -0300

    Added private constructor
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/subsistema1/crm/CrmService.java
+++ src/main/java/subsistema1/crm/CrmService.java
@@ -1,6 +1,10 @@
 package subsistema1.crm;
 
 public class CrmService {
+    private CrmService() {
+        super();
+    }
+
     public static void saveClient(String name, String postalCode, String city, String state) {
         System.out.println("Saved client into CRM system.");
     }
