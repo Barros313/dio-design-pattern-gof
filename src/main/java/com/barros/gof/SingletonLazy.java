ea50dcde880a595cf541271e977378143e51b3c9
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:06:17 -0300

    Added Singleton Lazy class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ src/main/java/com/barros/gof/SingletonLazy.java
@@ -0,0 +1,22 @@
+package com.barros.gof;
+
+/**
+ * Lazy Singleton
+ *
+ * @author Barros313
+ */
+public class SingletonLazy {
+    private static SingletonLazy instance;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstance() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+
+        return instance;
+    }
+}
---
+++ src/main/java/com/barros/gof/SingletonLazyHolder.java
@@ -0,0 +1,2 @@
+package com.barros.gof;public class SingletonLazyHolder {
+}
