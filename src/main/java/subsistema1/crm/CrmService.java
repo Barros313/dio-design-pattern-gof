36588220d12d1510165e40ba2c5c723bb0c03518
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 10:48:17 -0300

    Added crm package and class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ src/main/java/subsistema1/crm/CrmService.java
@@ -0,0 +1,7 @@
+package subsistema1.crm;
+
+public class CrmService {
+    public static void saveClient(String name, String postalCode, String city, String state) {
+        System.out.println("Saved client into CRM system.");
+    }
+}
