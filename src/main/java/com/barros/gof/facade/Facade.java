8a78f56bed25d0132bdc535a20510ee08dc54968
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-27 11:52:47 -0300

    Added facade class
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/barros/gof/facade/Facade.java
+++ src/main/java/com/barros/gof/facade/Facade.java
@@ -1,7 +1,13 @@
 package com.barros.gof.facade;
 
-public class facade {
-    public void migrateClient(String name, String postalCode) {
+import subsistema1.crm.CrmService;
+import subsistema2.cep.CepApi;
 
+public class Facade {
+    public void migrateClient(String name, String postalCode) {
+        String city = CepApi.getInstance().getCityThroughPostalCode(postalCode);
+        String state = CepApi.getInstance().getStateThroughPostalCode(postalCode);
+
+        CrmService.saveClient(name, postalCode, city, state);
     }
 }
